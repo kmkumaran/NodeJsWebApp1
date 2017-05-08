a6f09775d66b4fa3068f71d0cf38a6e9c2e86749
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2017-05-08 06:08:27 -0700

    Updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,39 @@
-var zip = require("gulp-zip");
 var gulp = require('gulp');
-var typescript = require('gulp-tsc');
+var path = require('path');
+var zip = require('gulp-zip');
+var minimist = require('minimist');
+var fs = require('fs');
 
-gulp.task("deploy", function () {
-    return gulp.src(["**"])
-        .pipe(zip("deploy.zip"))
-        .pipe(gulp.dest("zip"));
-});
+var knownOptions = {
+	string: 'packageName',
+	string: 'packagePath',
+	default: {packageName: "Package.zip", packagePath: path.join(__dirname, '_package')}
+}
 
-//gulp.task('deploy', () => {
-//return gulp.src('**')
-//		.pipe(zip('archive.zip'))
-//		.pipe(gulp.dest('./'));
-//});
-
-const fs = require('fs');
+var options = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('default', function () {
-    gulp.src(['**/*.ts', '!**/node_modules/**'])
-    .pipe(typescript())
-    .pipe(gulp.dest('./'))
+
+	var packagePaths = ['**', 
+					'!**/_package/**', 
+					'!**/typings/**',
+					'!typings', 
+					'!_package', 
+					'!gulpfile.js']
+	
+	//add exclusion patterns for all dev dependencies
+	var packageJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+	var devDeps = packageJSON.devDependencies;
+
+	for(var propName in devDeps)
+	{
+		var excludePattern1 = "!**/node_modules/" + propName + "/**";
+		var excludePattern2 = "!**/node_modules/" + propName;
+		packagePaths.push(excludePattern1);
+		packagePaths.push(excludePattern2);
+	}
+	
+    return gulp.src(packagePaths)
+        .pipe(zip(options.packageName))
+        .pipe(gulp.dest(options.packagePath));
 });
